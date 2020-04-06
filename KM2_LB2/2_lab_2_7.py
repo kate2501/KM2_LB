ad269cd601d919ff5ef05c45e33917b10dd5d52d
Kate B <dienstaggirl@gmail.com>
2020-04-06 20:42:04 +0300

    working on conditions 1

--- KM2_LB2/2_lab_2_7.py
+++ KM2_LB2/2_lab_2_7.py
@@ -1,8 +1,6 @@
 import argparse
 
-parser = argparse.ArgumentParser(description='leonardo number from cmd')
-parser.add_argument('integer', type=int, default=-1, help='Your number from cmd')
-args=parser.parse_args()
+__all__ = ['leonardo']
 
 def leonardo(n):
     a = [1, 1]
@@ -11,25 +9,36 @@ def leonardo(n):
         for i in range(2, n+1):
             a.append(a[i-1] + a[i-2] + 1)
     return a[-1]
+
 def check_val(val):
-    if isinstance(val,int):
+    try:
+        val = int(val)
         if val >= 0:
             return True
-    else:
         return False
+    except ValueError:
+        print('Wrong input!!!')
 
-if args.integer:
-    val = args.integer
-    if check_val(val):
-        print(leonardo(val))
-else:
-    print("Input 'exit' for exit")
-    while True:
-        val = input("Enter the number to find leonardo:")
-        if val == 'exit':
-            break
+
+def main():
+    parser = argparse.ArgumentParser(description='leonardo number from cmd')
+    parser.add_argument('-i', type=int, help='Your number from cmd')
+    args=parser.parse_args()
+    if args.i:
+        val = args.i
         if check_val(val):
             print(leonardo(val))
-        else:
-            continue
+    else:
+        print("'exit' for exit")
+        while True:
+            val = input("Enter the number to find leonardo:")
+            if val == 'exit':
+                break
+            elif check_val(val):
+                print(leonardo(int(val)))
+            else:
+                continue
+
+if __name__ == "__main__":
+    main()
 
--- KM2_LB2/2_lab_2_8.py
+++ KM2_LB2/2_lab_2_8.py
@@ -1,5 +1,40 @@
-def check_two(x):
-	return ((x != 0) and ((x & (~x + 1)) == x)) #the power of 2 shows the position of "1" bit
-	#return x > 0 and (x & (x - 1)) == 0
+import argparse
 
-print(check_two(1))
+__all__ = ['check_two']
+
+def check_two(x):
+	if str(bin(x)).count('1') == 1:
+		return len(bin(x)[3:])
+	return False
+
+def check_val(val):
+    try:
+        val = int(val)
+        return True
+    except ValueError:
+        return False
+        print('Wrong input!!!')
+
+
+def main():
+	parser = argparse.ArgumentParser(description='your number from cmd')
+	parser.add_argument('-i', type=int, help='Your number from cmd')
+	args=parser.parse_args()
+    if args.i:
+        val = args.i
+        if check_val(val):
+            print(check_two(int(val)))
+    else:
+        print("'exit' for exit")
+        while True:
+            val = input("Enter your number:")
+            if val == 'exit':
+                break
+            elif check_val(val):
+                print(check_two(int(val)))
+            else:
+                continue
+
+if __name__ == "__main__":
+    main()
+
