126a63c1b2c8ef0f926ba4625b6d38db6c3ce3fb
Kate B <dienstaggirl@gmail.com>
2020-04-06 21:01:04 +0300

    working on conditions 1

--- KM2_LB2/2_lab_2_1.py
+++ KM2_LB2/2_lab_2_1.py
@@ -1,6 +1,8 @@
 import math  
 import argparse
 
+__all__ = ['sqrt_decomp']
+
 def own_input():
     while True:
         print("Enter your array please")
@@ -18,7 +20,9 @@ def find_borders(arr, val):
             print("Only 2 borders!!!") 
         elif borders[0]>borders[1]:
             print("First left, then right") 
-        elif borders[0]<0 or borders[1]>len(arr):
+        elif borders[0]<0 or borders[1]<0:
+            print("Your borders don't correspond")
+        elif borders[0]>len(arr) or borders[1]>len(arr):
             print("Your borders don't correspond")
         else:
             return borders
@@ -33,8 +37,10 @@ def sqrt_decomp(a, l, r):
         b[i // new_len] += a[i]
     c_l, c_r  = l // new_len, r // new_len
     k = 0 
+    if l == r:
+        return a[l]
     if c_l == c_r:
-        for el in range(l, r + 1): 
+        for el in range(l, r): 
             k += a[el]
     else:
         for el in range(c_l + 1, c_r):
@@ -45,33 +51,34 @@ def sqrt_decomp(a, l, r):
             k += a[el]
     return k
 
-parser = argparse.ArgumentParser(description='Input from file')
-parser.add_argument('-f', type=str, help='Open file')
-args=parser.parse_args()
-
-if args.f:
-    try:
-        tmp = []
-        with open(args.f, 'r') as file:
-            for val in file.read().split():
-                try:
-                    tmp.append((int(val)))
-                except:
-                    continue
-        arr = tmp
-    except FileNotFoundError:
-            print('File does not exist')
-else:
-    arr = own_input()
-
-print("Input 'exit' for exit")
-while True:
-    val = input("Enter your borders(left,right) please:")
-    if val == 'exit':
-        break
-    if find_borders(arr, val):
-        borders = find_borders(arr, val)
-        print(sqrt_decomp(arr, borders[0], borders[1]))
+def main():
+    parser = argparse.ArgumentParser(description='Input from file')
+    parser.add_argument('-f', type=str, help='Open file')
+    args=parser.parse_args()
+    if args.f:
+        try:
+            tmp = []
+            with open(args.f, 'r') as file:
+                for val in file.read().split():
+                    try:
+                        tmp.append((int(val)))
+                    except:
+                        continue
+            arr = tmp
+        except FileNotFoundError:
+                print('File does not exist')
     else:
-        continue
+        arr = own_input()
+    print("Input 'exit' for exit")
+    while True:
+        val = input("Enter your borders(left,right) please:")
+        if val == 'exit':
+            break
+        if find_borders(arr, val):
+            borders = find_borders(arr, val)
+            print(sqrt_decomp(arr, borders[0], borders[1]))
+        else:
+            continue
 
+if __name__ == "__main__":
+    main()
