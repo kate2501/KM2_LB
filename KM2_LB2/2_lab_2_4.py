ed789882272525a95b5d6c9da5a927292c4e786b
Kate B <dienstaggirl@gmail.com>
2020-04-07 16:01:09 +0300

    changing completely

--- KM2_LB2/2_lab_2_4.py
+++ KM2_LB2/2_lab_2_4.py
@@ -1,31 +1,43 @@
-import sys 
-from collections.abc import Iterable
-import argparse
-
-parser = argparse.ArgumentParser(description='Input from file')
-parser.add_argument('Iterableobj', type=list, help='Open file')
-args=parser.parse_args()
-
+__all__ = ['flatten']
 
 def flatten_it(obj):
-    tmp = []
-    while obj:
-        s, k = obj[-1], obj[:-1]
-        obj = list(k)
-        if isinstance(s, Iterable):
-            obj.extend(s)
-        else:
-            tmp.append(s)
-    tmp.reverse()
-    return tmp
+    if isinstance(obj, list):
+        tmp = []
+        while obj:
+            s, k = obj[-1], obj[:-1]
+            obj = list(k)
+            if isinstance(s, (list, tuple, set)):
+                obj.extend(s)
+            elif isinstance(s, dict):
+                tmp.append(flatten_dict(s))
+            else:
+                tmp.append(s)
+        tmp.reverse()
+        return tmp
+    return obj
 
-ff = ((1,2),3,(([])))
-b = [[1], 2, [[3, 4], 5], [[[]]], [[[6],5],1], 7, 8]
-a = sys.argv[1]
-print(type(a)) 
-print(a)
-c = flatten_it(b)
-print(ff)
-print(b)
-print(c)
+def flatten(obj):
+    if isinstance(obj, dict):
+        return flatten_dict(obj)
+    return flatten_it(obj)
 
+def flatten_dict(d):
+    def expand(key, value):
+        if isinstance(value, dict):
+            return [ (str(key) + '_' + str(k), v) 
+                for k, v in flatten_dict(value).items() ]
+        return [ (key, flatten_it(value)) ]
+
+    items = [ item for k, v in d.items() for item in expand(k, v) ]
+
+    return dict(items)
+
+def main():
+    ini_dict = [("h", "h"),{'f': {1: {'for': ([(1,7, {1,(2,3)}),1,[], 
+            {'s': {'s1': {'s2': 3}}}, (9,0)])}}, 
+            'for': {'3': {3: 3}}, 
+            25: {'for': {'a': 1, 'for': 4}}}, [1,2, [5]]]
+    print(flatten(ini_dict))
+
+if __name__ == "__main__":
+    main()
